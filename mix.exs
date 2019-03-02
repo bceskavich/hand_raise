8846cac4670c2e2327124e516304a33d6df15655
Billy Ceskavich <bceskavich@gmail.com>
2019-03-02 09:04:05 -0500

    Add dialyzer

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule HandRaise.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false}
     ]
   end
 
